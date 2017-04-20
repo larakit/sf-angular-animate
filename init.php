8665e63f993ff7d7d6f86041a477fc24674bcbda
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 08:58:52 +0500

    Update init.php

--- init.php
+++ init.php
@@ -2,5 +2,4 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-animate')
     ->setSourceDir('public')
     ->usePackage('larakit/sf-angular')
-    ->ngModule('ngAnimate')
     ->jsPackage('angular-animate.min.js');
